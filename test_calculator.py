acc10f0b83e2e2fabde004db21814551eb022df8
ButtyMate00 <118981147+ButtyMate00@users.noreply.github.com>
2024-10-09 09:29:33 +0200

    add test_calculator.py with test functions

--- test_calculator.py
+++ test_calculator.py
@@ -14,3 +14,18 @@ def test_subtract():
      assert subtract(-6 ,-12) == 6
      assert subtract(10 ,-12) == 22
      assert subtract(0 ,0) == 0
+
+def test_multiply():
+     assert multiply(2 ,1) == 2
+     assert multiply(5 ,5) == 25
+     assert multiply(-6 ,-12) == 72
+     assert multiply(10 ,-12) == -120
+     assert multiply(0 ,0) == 0
+
+
+def test_divide():
+     assert divide(2 ,1) == 2
+     assert divide(5 ,5) == 1
+     assert divide(-6 ,-6) == 1
+     assert divide(10 ,-10) == -1
+     assert divide(1 ,1) == 1
